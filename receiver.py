210d5acdf3eb54ff91150e804cb26d05291c8182
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-19 10:33:23 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,6 +2,19 @@ import streamlit as st
 
 def app():
     st.title("Receiver App")
+    
+    if st.request.method == "POST":
+        username = st.request.form["username"]
+        password = st.request.form["password"]
+        st.session_state.received_username = username
+        st.session_state.received_password = password
+        st.write("Received Username:", username)
+        st.write("Received Password:", password)
+        
+        # You can redirect the user to another page after receiving the data
+        # using the `st.experimental_set_query_params` function.
+        st.experimental_set_query_params(username=username, password=password)
+
     received_username = st.session_state.get("received_username", None)
     received_password = st.session_state.get("received_password", None)
 
