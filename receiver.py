4245d4d30249d09e40be43f16d3c109308764769
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-19 11:03:10 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,27 +1,18 @@
 import streamlit as st
+import requests
 
 def app():
     st.title("Receiver App")
-    
-    if st.request.method == "POST":
-        username = st.request.form["username"]
-        password = st.request.form["password"]
-        st.session_state.received_username = username
-        st.session_state.received_password = password
-        st.write("Received Username:", username)
-        st.write("Received Password:", password)
-        
-        # You can redirect the user to another page after receiving the data
-        # using the `st.experimental_set_query_params` function.
-        st.experimental_set_query_params(username=username, password=password)
 
-    received_username = st.session_state.get("received_username", None)
-    received_password = st.session_state.get("received_password", None)
-
-    if received_username and received_password:
-        st.subheader("Received Data:")
-        st.write("Username:", received_username)
-        st.write("Password:", received_password)
+    if st.button("Receive Data"):
+        response = requests.post("http://localhost:8501", data={"username": "myusername", "password": "mypassword"})
+        if response.ok:
+            received_data = response.json()
+            st.subheader("Received Data:")
+            st.write("Username:", received_data.get("username"))
+            st.write("Password:", received_data.get("password"))
+        else:
+            st.write("Error:", response.status_code)
 
 if __name__ == "__main__":
     app()
