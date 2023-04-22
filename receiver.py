951a225a31bd46878e9ce17594faed89111d3084
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-22 14:35:42 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -3,7 +3,7 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("0.0.0.0", 9000))
+client.connect(("44.229.200.200", 200))
 
 st.subheader("received message")
 
